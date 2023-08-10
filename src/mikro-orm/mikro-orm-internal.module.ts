7b6bce3015be6ba8dcf6c072624f758cf2424d3b
Thiago Martins <thiago_valentim1@hotmail.com>
2023-08-10 11:19:49 -0300

    refactor: clean up module

--- src/mikro-orm/mikro-orm-internal.module.ts
+++ src/mikro-orm/mikro-orm-internal.module.ts
@@ -1,21 +1,16 @@
 import { MikroOrmModule } from '@mikro-orm/nestjs';
-import { DynamicModule, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 
-@Module({})
-export class MikroOrmInternalModule {
-  public static forTests(): DynamicModule {
-    return {
-      module: MikroOrmInternalModule,
-      imports: [
-        MikroOrmModule.forRoot({
-          type: 'postgresql',
-          clientUrl: 'postgresql://postgres:postgres@localhost:5435/postgres',
-          autoLoadEntities: true,
-          allowGlobalContext: true,
-          implicitTransactions: false,
-          debug: true,
-        }),
-      ],
-    };
-  }
-}
+@Module({
+  imports: [
+    MikroOrmModule.forRoot({
+      type: 'postgresql',
+      clientUrl: 'postgresql://postgres:postgres@localhost:5435/postgres',
+      autoLoadEntities: true,
+      allowGlobalContext: true,
+      implicitTransactions: false,
+      debug: true,
+    }),
+  ],
+})
+export class MikroOrmInternalModule {}
