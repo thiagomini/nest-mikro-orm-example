4f7bf39eb0707c4b57833015a9799af75a7fa383
Thiago Martins <thiago_valentim1@hotmail.com>
2023-09-07 11:13:26 -0300

    chore: remove extra code

--- src/mikro-orm/mikro-orm-internal.module.spec.ts
+++ src/mikro-orm/mikro-orm-internal.module.spec.ts
@@ -35,36 +35,4 @@ describe('MikroOrmInternalModule', () => {
     // Act
     await entityManager.persistAndFlush(user);
   });
-
-  test('creates a new user', async () => {
-    // Arrange
-    const entityManager = testingModule.get(EntityManager).fork();
-
-    const user = new User({
-      email: 'user2@mail.com',
-      firstName: 'John2',
-      lastName: 'Doe2',
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    });
-
-    // Act
-    await entityManager.persistAndFlush(user);
-  });
-
-  test('creates a new user', async () => {
-    // Arrange
-    const entityManager = testingModule.get(EntityManager).fork();
-
-    const user = new User({
-      email: 'user3@mail.com',
-      firstName: 'John3',
-      lastName: 'Doe3',
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    });
-
-    // Act
-    await entityManager.persistAndFlush(user);
-  });
 });
